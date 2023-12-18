85729d5a01dfffe2d23ca00b2966c77e6a4b66e9
lmiguelm <luismiguelfernandes.marcelo@gmail.com>
2023-12-18 12:15:34 -0300

    feat: adicionando storybook

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -1,13 +1,13 @@
-import React from 'react'
+import React from "react";
 
-import { styled } from './styles'
+import { styled } from "./styles";
 
-const Button = styled('button', {
-  fontFamily: '$default',
-  backgroundColor: '$ignite500',
-  borderRadius: '$md',
-})
+const Button = styled("button", {
+  fontFamily: "$default",
+  backgroundColor: "$ignite500",
+  borderRadius: "$md",
+});
 
 export function App() {
-  return <h1 style={{ color: colors.ignite300 }}>Hello World</h1>
+  return <Button>Hello world</Button>;
 }
