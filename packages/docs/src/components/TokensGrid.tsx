b304b4d092e83b6374e27771a5d3eaf512c400d7
lmiguelm <luismiguelfernandes.marcelo@gmail.com>
2023-12-18 23:29:02 -0300

    feat: documentando tokens

---
+++ packages/docs/src/components/TokensGrid.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+import "../styles/tokens-grid.css";
+
+interface TokensGridProps {
+  tokens: Record<string, string>;
+  hasRemValue?: boolean;
+}
+
+export function TokensGrid({ tokens, hasRemValue }: TokensGridProps) {
+  return (
+    <table className="tokens-grid">
+      <thead>
+        <th>Nome</th>
+        <th>Valor</th>
+        {hasRemValue && <th>Pixels</th>}
+      </thead>
+
+      <tbody>
+        {Object.entries(tokens).map(([key, value]) => {
+          return (
+            <tr key={key}>
+              <td>{key}</td>
+              <td>{value}</td>
+              {hasRemValue && (
+                <td>{Number(value.replace("rem", "")) * 16}px</td>
+              )}
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+}
