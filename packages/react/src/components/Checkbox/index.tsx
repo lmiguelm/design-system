18540ed2699763546c6a8eed1b2605717296e5d4
lmiguelm <luismiguelfernandes.marcelo@gmail.com>
2023-12-23 15:56:47 -0300

    feat: add componente checkbox

---
+++ packages/react/src/components/Checkbox/index.tsx
@@ -0,0 +1,15 @@
+import React, { ComponentProps } from 'react'
+import { CheckboxContainer, CheckboxIndicator } from './styles'
+import { Check } from 'phosphor-react'
+
+export type CheckBoxProps = ComponentProps<typeof CheckboxContainer>
+
+export function CheckBox(props: CheckBoxProps) {
+  return (
+    <CheckboxContainer {...props}>
+      <CheckboxIndicator asChild>
+        <Check weight="bold" />
+      </CheckboxIndicator>
+    </CheckboxContainer>
+  )
+}
