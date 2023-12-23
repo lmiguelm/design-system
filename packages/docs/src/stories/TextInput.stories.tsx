c18810e80df457a8696841cfe49a429d390d5871
lmiguelm <luismiguelfernandes.marcelo@gmail.com>
2023-12-23 18:35:26 -0300

    fix: melhorias nos componentes

--- packages/docs/src/stories/TextInput.stories.tsx
+++ packages/docs/src/stories/TextInput.stories.tsx
@@ -1,39 +1,40 @@
-import { Box, Text, TextInput, TextInputProps } from "@lmiguelm-ui/react";
-import type { Meta, StoryObj } from "@storybook/react";
-import React from "react";
+import { Box, Text, TextInput, TextInputProps } from '@lmiguelm-ui/react'
+import type { Meta, StoryObj } from '@storybook/react'
+import React from 'react'
 
 export default {
-  title: "Form/Text Input",
+  title: 'Form/Text Input',
   component: TextInput,
   decorators: [
     (Story) => {
       return (
         <Box
           as="label"
-          css={{ display: "flex", flexDirection: "column", gap: "$2" }}
+          css={{ display: 'flex', flexDirection: 'column', gap: '$2' }}
         >
           <Text size="sm">E-mail address</Text>
           {Story()}
         </Box>
-      );
+      )
     },
   ],
-} as Meta<TextInputProps>;
+} as Meta<TextInputProps>
 
 export const Primary: StoryObj<TextInputProps> = {
   args: {
-    placeholder: "Type your name",
+    placeholder: 'Type your name',
   },
-};
+}
 
 export const Disabled: StoryObj<TextInputProps> = {
   args: {
     disabled: true,
   },
-};
+}
 
 export const WithPrefix: StoryObj<TextInputProps> = {
   args: {
-    prefix: "call.com/",
+    prefix: 'call.com/',
+    placeholder: 'your-username',
   },
-};
+}
---
+++ packages/react/CHANGELOG.md
@@ -0,0 +1,7 @@
+# @lmiguelm-ui/react
+
+## 1.0.1
+
+### Patch Changes
+
+- Adicioanndo algumas melhorias para o projeto
